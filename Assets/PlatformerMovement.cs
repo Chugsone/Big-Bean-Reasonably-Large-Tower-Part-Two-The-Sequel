fcc22267c8625535f24e0c375a34b056f4f6b057
MUKILTEO\1726409 <r.francis.yeet@outlook.com>
2025-10-20 13:39:27 -0700

    started movement script

---
+++ Assets/PlatformerMovement.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class PlatformerMovement : MonoBehaviour
+{
+    public float speed;
+    public float jumpheight;
+
+    public Rigidbody2D rb2d;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Move()
+        {
+        
+        }
+
+    public void Jump()
+    {
+
+    }
+
+
+}
