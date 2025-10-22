6920ad6f978b5ddbd68181f7252afa6a934ddf57
MUKILTEO\1726619 <1726619@mukilteo.wednet.edu>
2025-10-22 13:40:07 -0700

    Added animation

--- Assets/PlatformerMovement.cs
+++ Assets/PlatformerMovement.cs
@@ -9,28 +9,31 @@ public class PlatformerMovement : MonoBehaviour
     public Rigidbody2D rb2d;
 
     private float movement;
+    public Animator anim;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+ 
     }
 
     // Update is called once per frame
     void Update()
     {
         rb2d.linearVelocityX = movement;
-        
+        anim.SetFloat("HorizantalSpeed", movement);
     }
 
     public void Move(InputAction.CallbackContext ctx)
-        {
+    {
             movement = ctx.ReadValue<Vector2>().x * moveSpeed;
     }
 
     public void Jump(InputAction.CallbackContext ctx)
     {
-        rb2d.linearVelocityY = jumpHeight;
+        if(ctx.canceled) return;
+        rb2d.AddForceY(jumpHeight);
+        anim.SetTrigger("Jump");
     }
 
 
