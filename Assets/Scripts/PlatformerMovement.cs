0723764e5b87d92bd9cd553a0f4a3faa03fad3d9
MUKILTEO\1726619 <1726619@mukilteo.wednet.edu>
2025-10-23 11:26:56 -0700

    biggest planet in the world

--- Assets/Scripts/PlatformerMovement.cs
+++ Assets/Scripts/PlatformerMovement.cs
@@ -14,11 +14,7 @@ public class PlatformerMovement : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
-<<<<<<< HEAD:Assets/PlatformerMovement.cs
- 
-=======
-        rb2d = GetComponent<Rigidbody2D>();
->>>>>>> c17ed7d74f4460d7068323a9547da69ffd6adc0d:Assets/Scripts/PlatformerMovement.cs
+
     }
 
     // Update is called once per frame
@@ -35,14 +31,6 @@ public class PlatformerMovement : MonoBehaviour
 
     public void Jump(InputAction.CallbackContext ctx)
     {
-<<<<<<< HEAD:Assets/PlatformerMovement.cs
-        if(ctx.canceled) return;
-        rb2d.AddForceY(jumpHeight);
-        anim.SetTrigger("Jump");
-=======
-        if (ctx.ReadValue<float>() == 1)
 
-        rb2d.linearVelocityY = jumpHeight;
->>>>>>> c17ed7d74f4460d7068323a9547da69ffd6adc0d:Assets/Scripts/PlatformerMovement.cs
     }
 }
