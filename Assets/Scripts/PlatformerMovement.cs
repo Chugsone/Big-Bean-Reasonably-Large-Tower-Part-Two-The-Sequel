e9d2e88ba1a9f49aa1075044e849feb3d58c24ed
MUKILTEO\1726409 <r.francis.yeet@outlook.com>
2025-10-27 13:35:41 -0700

    I hate particles

--- Assets/Scripts/PlatformerMovement.cs
+++ Assets/Scripts/PlatformerMovement.cs
@@ -1,18 +1,25 @@
+using JetBrains.Annotations;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
-
+using System.Collections;
+using Unity.VisualScripting;
 public class PlayerMovment : MonoBehaviour
 {
+    public float Delay1 =9;
     public float speed;
     public float JumpHeight;
     private Rigidbody2D Rb;
     private float _movement;
+    private float direction=1;
     public Vector2 boxsize;
     public float castDistance;
     public LayerMask groundLayer;
     public GameObject playerObj;
-
+    public float dashForce;
+    public float dashTime;
+    public GameObject dashEffect;
+    public float dashCooldown;
 
     // grabs inital position
     private Vector2 InitialPos;
@@ -20,26 +27,75 @@ public class PlayerMovment : MonoBehaviour
     // grabs initial rotation
     private Quaternion InitialRot;
 
+    public bool isDashing;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
         Rb = GetComponent<Rigidbody2D>();
     }
+    /*private IEnumerator DelayedActionCoroutine(float delay1)
+    {
+        Debug.Log("Waiting for " + delay1 + " seconds...");
 
+        // Pause the function for the specified time
+        yield return new WaitForSeconds(delay1);
+
+        // This code will run after the delay
+        Debug.Log("Delay finished! Action performed.");
+        dashEffect.SetActive(false);
+    }
+    */
     // Update is called once per frame
     void Update()
     {
-        Rb.linearVelocityX = _movement;
+       
+        
+        if (isDashing==false)
+        {
+            //StartCoroutine(DelayedActionCoroutine(3.0f));
+            
+            Rb.linearVelocityX = _movement;
+            dashEffect.SetActive(false);
+
+        }
+        if (isDashing==true)
+        {
+            
+            dashEffect.SetActive(true);
+        }
+
+        
+
+
+        if (Input.GetKeyDown(KeyCode.Q) && !isDashing)
+        {
+            Rb.AddForce (new Vector2(dashForce*direction, 0f));
+            isDashing = true;
+
+            Invoke(nameof(EndDash), dashTime);
+        }
     }
 
+    private void DashCooldown(float coolDown)
+    {
 
+    }
+
+    private void EndDash()
+    {
+        isDashing = false;
+    }
 
     // Crontrolls the players horizontal movment
     public void Move(InputAction.CallbackContext ctx)
     {
 
         _movement = ctx.ReadValue<Vector2>().x * speed;
+
+        if (_movement != 0)
+            direction = Mathf.Sign(_movement);
 
     }
     // Controlls the players jump and makes sure the player is grounded
