d409f784936a0c0de008ab6ca560cfa9065bc6b4
MUKILTEO\1726377 <chasebannavong@gmail.com>
2025-10-31 13:13:41 -0700

    [Verse 1: Nate Ruess] Give me a second I, I need to get my story straight My friends are in the bathroom getting higher than the Empire State My lover, she is waiting for me just across the bar My seat's been taken by some sunglasses askin' 'bout a scar, and I know I gave it to you months ago I know you're tryin' to forget But between the drinks and subtle things The holes in my apologies You know, I'm tryin' hard to take it back
    [Pre-Chorus: Nate Ruess]
    So if by the time the bar closes
    And you feel like fallin' down
    I'll carry you home
    
    [Chorus: Nate Ruess]
    Tonight
    We are young
    So let's set the world on fire
    We can burn brighter than the sun
    Tonight
    We are young
    So let's set the world on fire
    We can burn brighter than the sun
    
    [Verse 2: Nate Ruess]
    Now I know that I'm not all that you got
    I guess that I, I just thought
    Maybe we could find new ways to fall apart
    But our friends are back, so let's raise a toast
    'Cause I found someone to carry me home
    
    [Chorus: Nate Ruess]
    Tonight
    We are young
    So let's set the world on fire
    We can burn brighter than the sun
    Tonight
    We are young
    So let's set the world on fire
    We can burn brighter than the sun
    
    [Bridge: Janelle Monáe & Nate Ruess]
    Carry me home tonight
    (Na-na, na-na-na-na-na-na, na-na-na-na)
    Just carry me home tonight
    (Na-na, na-na-na-na-na-na, na-na-na-na)
    Carry me home tonight
    (Na-na, na-na-na-na-na-na, na-na-na-na)
    Just carry me home tonight
    (Na-na, na-na-na-na-na-na, na-na-na-na)
    (Na-na, na-na-na-na-na-na, na-na-na-na)
    The moon is on my side, I have no reason to run
    (Na-na, na-na-na-na-na-na, na-na-na-na)
    So will someone come and carry me home tonight?
    (Na-na, na-na-na-na-na-na, na-na-na-na)
    The angels never arrived, but I can hear the choir
    (Na-na, na-na-na-na-na-na, na-na-na-na)
    So will someone come and carry me home?
    
    [Chorus: Nate Ruess]
    Tonight
    We are young
    So let's set the world on fire
    We can burn brighter than the sun
    Tonight
    We are young
    So let's set the world on fire
    We can burn brighter than the sun
    
    [Outro: Nate Ruess]
    So if by the time the bar closes
    And you feel like fallin' down
    I'll carry you home tonight

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,75 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public Transform player;
+    public float chaseSpeed = 2f;
+    public float jumpForce = 5f;
+    public LayerMask groundLayer;
+
+
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    private bool shouldJump;
+   
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    
+    void Update()
+    {
+        // Is Grounded
+        isGrounded  = Physics2D.Raycast(transform.position, Vector2.down, 1f, groundLayer);
+        // Player Direction
+        float direction = Mathf.Sign(player.position.x - transform.position.x);
+        // Player above detection
+        bool isPlayerAbove = Physics2D.Raycast(transform.position, Vector2.up, 3f, 1 << player.gameObject.layer);
+
+        if (isGrounded)
+        {
+           // chase player
+           rb.linearVelocity = new Vector2(direction * chaseSpeed, rb.linearVelocity.y);
+
+
+            RaycastHit2D groundInFront = Physics2D.Raycast(transform.position, new Vector2(direction, 0), 2f, groundLayer);
+
+            RaycastHit2D gapAhead = Physics2D.Raycast(transform.position + new Vector3(direction * 1f, 0, 0), Vector2.down, 2f, groundLayer);
+
+            RaycastHit2D platformAbove = Physics2D.Raycast(transform.position, Vector2.up, 3f, groundLayer);
+
+
+            if (!groundInFront.collider && !gapAhead.collider)
+            {
+                shouldJump = true;
+            }
+            else if (isPlayerAbove && platformAbove.collider)
+            {
+                shouldJump = true;
+            }
+           
+        }
+
+
+
+    }
+    private void FixedUpdate()
+    {
+        if(isGrounded && shouldJump)
+        {
+            shouldJump = false;
+            Vector2 direction = (player.position - transform.position).normalized;
+
+
+            Vector2 jumpDirection = direction * jumpForce;
+
+            rb.AddForce(new Vector2(jumpDirection.x, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+}
+
